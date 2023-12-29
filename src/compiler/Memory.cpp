53a55048d3ebbc42a3865d9d5fe93efa200f6974
james <mrjameshamilton@gmail.com>
2023-12-29 12:55:19 +0100

    Remove space

--- src/compiler/Memory.cpp
+++ src/compiler/Memory.cpp
@@ -122,7 +122,7 @@ namespace lox {
 
 
         Builder->SetInsertPoint(IsStringBlock);
-# if DEBUG_LOG_GC
+#if DEBUG_LOG_GC
         static const auto fmt = Builder->CreateGlobalStringPtr("free '%s' @ %p\n");
         Builder->PrintF({fmt, Builder->AsCString(value), value});
         //Builder->CreateFree(); TODO: free string chars? but they're not allocated by malloc.
