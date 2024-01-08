aeeb5c78a568558ac87500d3c5130d2411a9ce1f
james <mrjameshamilton@gmail.com>
2024-01-08 22:28:43 +0100

    Use constexpr instead of define for NaN boxing values

--- src/compiler/Value.h
+++ src/compiler/Value.h
@@ -3,18 +3,18 @@
 
 #include <cstdint>
 
-#define SIGN_BIT ((uint64_t) 0x8000000000000000)
-#define QNAN ((uint64_t) 0x7ffc000000000000)
+constexpr uint64_t SIGN_BIT = 0x8000000000000000;
+constexpr uint64_t QNAN = 0x7ffc000000000000;
 
-#define TAG_UNINITIALIZED 0
-#define TAG_NIL 1
-#define TAG_FALSE 2
-#define TAG_TRUE 3
+constexpr uint64_t TAG_UNINITIALIZED = 0;
+constexpr uint64_t TAG_NIL = 1;
+constexpr uint64_t TAG_FALSE = 2;
+constexpr uint64_t TAG_TRUE = 3;
 
-#define FALSE_VAL ((uint64_t) (QNAN | TAG_FALSE))
-#define TRUE_VAL ((uint64_t) (QNAN | TAG_TRUE))
-#define NIL_VAL ((uint64_t) (QNAN | TAG_NIL))
-#define UNITIALIZED_VAL ((uint64_t) (QNAN | TAG_UNINITIALIZED))
+constexpr uint64_t FALSE_VAL = QNAN | TAG_FALSE;
+constexpr uint64_t TRUE_VAL = QNAN | TAG_TRUE;
+constexpr uint64_t NIL_VAL = QNAN | TAG_NIL;
+constexpr uint64_t UNITIALIZED_VAL = QNAN | TAG_UNINITIALIZED;
 
 namespace lox {
     enum class ObjType : int8_t {
