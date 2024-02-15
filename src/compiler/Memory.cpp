eab283c771e9656b22d854c21763871291fd5df0
james <mrjameshamilton@gmail.com>
2024-02-15 21:31:17 +0100

    Use getPtrTy for malloc

--- src/compiler/Memory.cpp
+++ src/compiler/Memory.cpp
@@ -24,7 +24,7 @@ namespace lox {
 
         const auto NewObjMalloc = CreateMalloc(
             IntPtrTy,
-            StructType,
+            getPtrTy(),
             allocsize,
             nullptr
         );
