4ff54e3a5f811bb19e71f892ffc8532cf1c5dee9
james <mrjameshamilton@gmail.com>
2024-02-17 11:37:20 +0100

    Extract allocateFunction helper

--- src/compiler/Function.cpp
+++ src/compiler/Function.cpp
@@ -3,15 +3,51 @@
 namespace lox {
 
     Value *LoxBuilder::AllocateFunction(llvm::Function *Function, const bool isNative) {
-        Value *ptr = AllocateObj(ObjType::FUNCTION, Function->getName());
-        Value *name = AllocateString(CreateGlobalStringPtr(Function->getName()), getInt32(Function->getName().size()), "funcName_" + std::string(Function->getName()));
+        static auto AllocateFunctionFunction([this] {
+            const auto F = Function::Create(
+                FunctionType::get(
+                    getPtrTy(),
+                    {getPtrTy(), getPtrTy(), getInt32Ty(), getInt32Ty(), getInt1Ty()},
+                    false
+                ),
+                Function::InternalLinkage,
+                "$allocateFunction",
+                getModule()
+            );
 
-        CreateStore(getInt32(Function->arg_size() - 1), CreateObjStructGEP(ObjType::FUNCTION, ptr, 1));
-        CreateStore(Function, CreateObjStructGEP(ObjType::FUNCTION, ptr, 2, "funcPtr"));
-        CreateStore(name, CreateObjStructGEP(ObjType::FUNCTION, ptr, 3));
-        CreateStore(isNative ? getTrue() : getFalse(), CreateObjStructGEP(ObjType::FUNCTION, ptr, 4));
+            LoxBuilder B(getContext(), getModule(), *F);
 
-        return ptr;
+            const auto EntryBasicBlock = B.CreateBasicBlock("entry");
+            B.SetInsertPoint(EntryBasicBlock);
+
+            const auto arguments = F->args().begin();
+
+            const auto functionPtr = arguments;
+            const auto namePtr = arguments + 1;
+            const auto nameLength = arguments + 2;
+            const auto argSize = arguments + 3;
+            const auto isNative = arguments + 4;
+
+            const auto ptr = B.AllocateObj(ObjType::FUNCTION, "function");
+            const auto name = B.AllocateString(namePtr, nameLength, "name");
+
+            B.CreateStore(argSize, B.CreateObjStructGEP(ObjType::FUNCTION, ptr, 1), "argSize");
+            B.CreateStore(functionPtr, B.CreateObjStructGEP(ObjType::FUNCTION, ptr, 2, "funcPtr"));
+            B.CreateStore(name, B.CreateObjStructGEP(ObjType::FUNCTION, ptr, 3), "name");
+            B.CreateStore(isNative, B.CreateObjStructGEP(ObjType::FUNCTION, ptr, 4), "isNative");
+
+            B.CreateRet(ptr);
+
+            return F;
+        }());
+
+        return CreateCall(
+            AllocateFunctionFunction,
+            {Function,
+             CreateGlobalStringPtr(Function->getName()), getInt32(Function->getName().size()), getInt32(Function->arg_size() - 1),
+             isNative ? getTrue() : getFalse()
+            }
+        );
     }
 
     Value *LoxBuilder::AllocateClosure(llvm::Function *F, bool isNative) {
