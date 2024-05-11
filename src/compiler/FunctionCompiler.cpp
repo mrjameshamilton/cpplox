9fd47488db5773cd4a6a337a89eff124abccb460
james <mrjameshamilton@gmail.com>
2024-05-11 13:35:24 +0200

    Add function debug logging

--- src/compiler/FunctionCompiler.cpp
+++ src/compiler/FunctionCompiler.cpp
@@ -1,4 +1,5 @@
 #include "FunctionCompiler.h"
+#include "../Debug.h"
 #include "ModuleCompiler.h"
 
 namespace lox {
@@ -6,9 +7,19 @@ namespace lox {
     void FunctionCompiler::compile(const std::vector<Stmt> &statements, const std::vector<Token> &parameters, const std::function<void(LoxBuilder &)> &entryBlockBuilder) {
 
         Builder.SetInsertPoint(EntryBasicBlock);
+        if constexpr (DEBUG_LOG_GC) {
+            Builder.PrintF({
+                Builder.CreateGlobalCachedString("# start function %s\n"),
+                Builder.CreateGlobalCachedString(Builder.getFunction()->getName()),
+            });
+        }
 
         // At the beginning of the function, remember the current local variable stack pointer.
         Builder.CreateStore(Builder.getModule().getLocalsStack()->getCount(Builder), sp);
+
+        if constexpr (DEBUG_LOG_GC) {
+            Builder.PrintF({Builder.CreateGlobalCachedString("\tset sp = %d\n"), Builder.CreateLoad(Builder.getInt32Ty(), sp)});
+        }
 
         beginScope();
         {
@@ -21,6 +32,9 @@ namespace lox {
 
             beginScope();
             {
+                if constexpr (DEBUG_LOG_GC) {
+                    Builder.PrintF({Builder.CreateGlobalCachedString("## start function inner scope %s (sp %d)\n"), Builder.CreateGlobalCachedString(Builder.getFunction()->getName()), Builder.CreateLoad(Builder.getInt32Ty(), sp)});
+                }
                 // Declare parameters and store them in local variables.
                 auto arg = Builder.getFunction()->arg_begin() + 2 /* second arg is receiver, first is upvalues array */;
                 for (auto &p: parameters) {
@@ -44,6 +58,10 @@ namespace lox {
             }
             endScope();
 
+            if constexpr (DEBUG_LOG_GC) {
+                Builder.PrintF({Builder.CreateGlobalCachedString("## end function inner scope %s (sp %d)\n"), Builder.CreateGlobalCachedString(Builder.getFunction()->getName()), Builder.CreateLoad(Builder.getInt32Ty(), sp)});
+            }
+
             const auto ReturnBlock = Builder.CreateBasicBlock("exit");
             Builder.CreateBr(ReturnBlock);
             Builder.SetInsertPoint(ReturnBlock);
@@ -53,6 +71,10 @@ namespace lox {
         // At the end of the function, reset the stack pointer then any variables allocated
         // in the function are no longer accessible as GC roots and can be freed.
         Builder.getModule().getLocalsStack()->setCount(Builder, Builder.CreateLoad(Builder.getInt32Ty(), sp));
+
+        if constexpr (DEBUG_LOG_GC) {
+            Builder.PrintF({Builder.CreateGlobalCachedString("# end function scope %s (sp %d)\n"), Builder.CreateGlobalCachedString(Builder.getFunction()->getName()), Builder.CreateLoad(Builder.getInt32Ty(), sp)});
+        }
 
         assert(scopes.empty());
 
